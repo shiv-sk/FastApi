124138016f0e9babc9cdf03d95dc2be1c892932e
shivanand-k <shivanandcrew034@gmail.com>
2025-10-09 20:34:18 +0530

    database url updated

--- new-todo/database.py
+++ new-todo/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql+psycopg2://postgres:SQL%40872@localhost:5432/todo"
+DATABASE_URL = "db_url"
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
